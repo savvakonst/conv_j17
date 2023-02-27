3f2d631497da3bcc2c40b9925455a49251331cdb
out-stroki-waisman <k5LpLsM5Kgjy>
2023-02-27 14:25:48 +0300

    Added calculate diff func

--- src/main/java/org/example/domain/Axis.java
+++ src/main/java/org/example/domain/Axis.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Data
@@ -12,4 +14,23 @@ import java.util.List;
 public class Axis {
 	private int x;
 	private List<Segment> segments;
+
+	public static List<Segment> calculateDiff(Segment seg1, Segment seg2) {
+		if (seg1.getY1() < seg2.getY1() && seg2.getY2() < seg1.getY2()) {
+			return new ArrayList<>(List.of(
+					new Segment(seg1.getY1(), seg2.getY1()),
+					new Segment(seg2.getY2(), seg1.getY1())
+			));
+		}
+		if (seg1.getY2() > seg2.getY1()) {
+			return new ArrayList<>(Collections.singletonList(seg1));
+		}
+		if (seg1.getY1() < seg2.getY1() && seg2.getY2() > seg1.getY1()) {
+			return new ArrayList<>(Collections.singletonList(new Segment(seg1.getY1(), seg2.getY1())));
+		}
+		if (seg1.getY1() > seg2.getY1() && seg1.getY2() < seg2.getY2()) {
+			return new ArrayList<>(Collections.emptyList());
+		}
+		throw new RuntimeException("Не возможно произвести расчеты");
+	}
 }
