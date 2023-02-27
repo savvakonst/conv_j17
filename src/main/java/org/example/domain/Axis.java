939caeff7629e4c05dac0f75ba6f8e3b06a865c1
out-stroki-waisman <k5LpLsM5Kgjy>
2023-02-27 15:42:22 +0300

    Added calculate diff func

--- src/main/java/org/example/domain/Axis.java
+++ src/main/java/org/example/domain/Axis.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 @Data
 @AllArgsConstructor
@@ -15,22 +13,47 @@ public class Axis {
 	private int x;
 	private List<Segment> segments;
 
-	public static List<Segment> calculateDiff(Segment seg1, Segment seg2) {
-		if (seg1.getY1() < seg2.getY1() && seg2.getY2() < seg1.getY2()) {
-			return new ArrayList<>(List.of(
-					new Segment(seg1.getY1(), seg2.getY1()),
-					new Segment(seg2.getY2(), seg1.getY1())
-			));
+//	public static List<Segment> calculateDiff(Segment seg1, Segment seg2) {
+//		int x1 = seg1.getY1();
+//		int x2 = seg1.getY2();
+//		int y1 = seg2.getY1();
+//		int y2 = seg2.getY2();
+//
+//		if (x1 == y1) {
+//			if (x2 == y2) return Collections.singletonList(new Segment(y2, x2));
+//			else return Collections.emptyList();
+//		}
+//		if (x1 < y1) {
+//			if (x2 == y2) return Collections.singletonList(new Segment(x1, y1));
+//			if (x2 > y2) return List.of(new Segment(x1, y1), new Segment(y2, x2));
+//			else return Collections.singletonList(new Segment(x1, y1));
+//		}
+//		if (x1 > y1) {
+//			if (x2 > y2) return Collections.singletonList(new Segment(y2, x2));
+//			else return Collections.emptyList();
+//		}
+//		throw new RuntimeException("Не возможно произвести расчеты");
+//	}
+
+	public static List<Segment> calculateDiff(Segment a, Segment b) {
+		if (b.getY1() <= a.getY1()) {
+			if (a.getY2() <= b.getY2()) // it needs only once
+				return new ArrayList<>(Collections.emptyList());
+
+			if (b.getY2() <= a.getY1())
+				return Collections.singletonList(a);
+			else
+				return new ArrayList<>(Collections.singletonList(new Segment(b.getY2(), a.getY2())));
 		}
-		if (seg1.getY2() > seg2.getY1()) {
-			return new ArrayList<>(Collections.singletonList(seg1));
+		if (a.getY2() <= b.getY2()) {
+			if (a.getY2() <= b.getY1())
+				return Collections.singletonList(a);
+			else
+				return new ArrayList<>(Collections.singletonList(new Segment(a.getY1(), b.getY1())));
 		}
-		if (seg1.getY1() < seg2.getY1() && seg2.getY2() > seg1.getY1()) {
-			return new ArrayList<>(Collections.singletonList(new Segment(seg1.getY1(), seg2.getY1())));
-		}
-		if (seg1.getY1() > seg2.getY1() && seg1.getY2() < seg2.getY2()) {
-			return new ArrayList<>(Collections.emptyList());
-		}
-		throw new RuntimeException("Не возможно произвести расчеты");
+		return new ArrayList<>(List.of(
+				new Segment(a.getY1(), b.getY1()),
+				new Segment(b.getY2(), a.getY1())
+		));
 	}
 }
